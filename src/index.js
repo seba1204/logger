76e42864465a11438ac3c76ee1ff24cc6c647c5f
seba12 <seba12.04@orange.fr>
2020-11-24 05:19:11 +0100

    changing color

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import emoji from "node-emoji";
-import chalk from "chalk";
-import moment from "moment";
+import emoji from 'node-emoji'
+import chalk from 'chalk'
+import moment from 'moment'
 
-let showTime = false;
-let setTime = "HH:mm:ss";
-let clearOnce = true;
-let clear = false;
+const showTime = false
+const setTime = 'HH:mm:ss'
+const clearOnce = true
+const clear = false
 
 export const settings = {
   showTime,
@@ -14,14 +14,16 @@ export const settings = {
   clear,
 }
 
-export default ({ name, mode, status, value, clear, showTime }) => {
+export default ({
+  name, mode, status, value, clear, showTime
+}) => {
   // definition of styles to apply
   const s = {
     regular: chalk.dim.white,
     error: chalk.bold.red,
-    warning: chalk.keyword("orange"),
-    success: chalk.keyword("green").bold,
-    info: chalk.keyword("cyan").bold,
+    warning: chalk.keyword('orange'),
+    success: chalk.keyword('greenBright').bold,
+    info: chalk.keyword('cyan').bold,
     devMode: chalk.cyan,
     prodMode: chalk.magenta,
     time: chalk.grey,
@@ -29,66 +31,66 @@ export default ({ name, mode, status, value, clear, showTime }) => {
 
   // definition of emoji to display
   const e = {
-    success: emoji.get("heavy_check_mark"),
-    error: emoji.get("x"),
-    info: emoji.get("grey_exclamation"),
-    warning: emoji.get("warning"),
-    waiting: emoji.get("hourglass"),
+    success: emoji.get('heavy_check_mark'),
+    error: emoji.get('x'),
+    info: emoji.get('grey_exclamation'),
+    warning: emoji.get('warning'),
+    waiting: emoji.get('hourglass'),
   }
 
-  let emojiToDisplay = e.warning;
-  let globalStyle = s.regular;
-  let modeStyle = s.regular;
-  const timeStyle = s.time;
+  let emojiToDisplay = e.warning
+  let globalStyle = s.regular
+  let modeStyle = s.regular
+  const timeStyle = s.time
 
   switch (status) {
-    case "ok":
-      globalStyle = s.success;
-      emojiToDisplay = e.success;
-      break;
-    case "warn":
-      globalStyle = s.warning;
-      emojiToDisplay = e.warning;
-      break;
-    case "err":
-      globalStyle = s.error;
-      emojiToDisplay = e.error;
-      break;
-    case "reg":
-      globalStyle = s.regular;
-      emojiToDisplay = e.success;
-      break;
-    case "wait":
-      globalStyle = s.regular;
-      emojiToDisplay = e.waiting;
-      break;
-    case "info":
-      globalStyle = s.info;
-      emojiToDisplay = e.info;
-      break;
+    case 'ok':
+      globalStyle = s.success
+      emojiToDisplay = e.success
+      break
+    case 'warn':
+      globalStyle = s.warning
+      emojiToDisplay = e.warning
+      break
+    case 'err':
+      globalStyle = s.error
+      emojiToDisplay = e.error
+      break
+    case 'reg':
+      globalStyle = s.regular
+      emojiToDisplay = e.success
+      break
+    case 'wait':
+      globalStyle = s.regular
+      emojiToDisplay = e.waiting
+      break
+    case 'info':
+      globalStyle = s.info
+      emojiToDisplay = e.info
+      break
     default:
-      globalStyle = s.error;
-      emojiToDisplay = e.error;
-      break;
+      globalStyle = s.error
+      emojiToDisplay = e.error
+      break
   }
 
   switch (mode) {
-    case "dev":
-      modeStyle = s.devMode;
-      break;
-    case "prod":
-      modeStyle = s.prodMode;
-      break;
+    case 'dev':
+      modeStyle = s.devMode
+      break
+    case 'prod':
+      modeStyle = s.prodMode
+      break
   }
 
-  !mode ? (mode = "") : (mode = `[${mode}] `);
-  !name ? (name = "") : (name = `[${name}] `);
-  !value ? (value = "") : (value = value);
+  !mode ? (mode = '') : (mode = `[${mode}] `)
+  !name ? (name = '') : (name = `[${name}] `)
+  !value ? (value = '') : (value = value)
   if (clear || settings.clear || settings.clearOnce) {
     console.clear()
   }
   if (settings.clearOnce) settings.clearOnce = false
-  let time = ""
+  let time = ''
   if (showTime || settings.showTime) {
     time = `${moment().format(settings.setTime)} `
   }
